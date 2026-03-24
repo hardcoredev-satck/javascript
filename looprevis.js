9c412b119a6e4c57478235c00d495a865e615541
hardcoredev-satck <darshitrana2213@gmail.com>
2026-03-24 23:44:11 +0530

    Add files via upload

---
+++ looprevis.js
@@ -0,0 +1,62 @@
+// simple rember this i starting valuee 
+// for (let i = 1; i <= n; i++) {
+// logic 
+
+/*i = start
+ i <= end
+ i++ → increment*/
+
+
+// print 1 to 10 
+/*
+    for (let i =1 ; i<=10 ; i++){
+        alert(i);
+    } */
+
+// even number from 1 to 10
+
+/*for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0)
+        alert(i);
+}*/
+
+
+// Sum of first 10 natural numbers. 
+/* let n =0
+for(let i = 1 ; i<=10; i++){
+ n= n+i;
+
+}
+ alert(n);*/
+
+
+ // Sum of even numbers from 1–n. 
+ // que no mltb n = 10 t0 1thi 10 ni ander evevn number no sum
+ /* let n = 0;
+ for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0)
+         n = n+i;   
+}
+alert(n); */
+
+
+// . Multiplication table of any number.
+/*
+let n= 5;
+
+for(let i = 1; i<=10 ; i++){
+    p= n*i;
+    alert(p);
+} */
+
+    //Factorial of a number. 
+
+    let n = 144;
+    let fat= 1;
+    for(let i=1; i<=n; i++){
+      fat = fat*i ;
+
+       
+    }
+
+     alert(p);
